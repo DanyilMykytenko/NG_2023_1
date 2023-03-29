1e1c8e79086a24e46af776381aa48cbd2c65d96a
DaniylMykytenko <freezeee682@gmail.com>
2023-03-29 03:24:38 +0300

    Sometimes I am open-mind person, sometime - close-mind. But now im magic-mind!

---
+++ SolidTasks/Open-Closed/Open-Closed/MagicClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed
+{
+    public class MagicClass
+    {
+        public void CountYourMagic(int magic) 
+        {
+            if(magic == 150)
+            {
+                Console.WriteLine("Wow, your magic is fire magic!");
+                return;
+            }
+            if(magic == 50000000)
+            {
+                Console.WriteLine("Incredible! You have 50 millions of power! It's water magic!");
+                return;
+            }
+            Console.WriteLine("I understand you...");
+        }
+    }
+}
