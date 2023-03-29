cda414df23c05a1fb3a14351c7aae3a2a98b6d0b
DaniylMykytenko <freezeee682@gmail.com>
2023-03-29 03:31:21 +0300

    Liskov-Barbara Substitution is here. But is Liskov-Barbara in the same room?

---
+++ SolidTasks/LiskovSubstitution/LiskovSubstitution/IWorkingWithFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public interface IWorkingWithFile
+    {
+        public string ReadFromFile(string filename);
+        public void WriteToFile(string filename);
+        public void DeleteFile(string filename);
+        public void DownloadFile(string filename);
+        public void CopyFile(string filename);
+        public void GetDataFromFile(string filename);
+        public void CheckFile(string filename);
+        public void SaveToFile(string filename);
+        public Guid CheckRole(Guid role);
+        public Guid CheckUser(Guid user);
+    }
+}
