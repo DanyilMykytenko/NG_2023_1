67e9406f2815c1de88e6f6cfe67c8c04a7da7a24
DaniylMykytenko <freezeee682@gmail.com>
2023-04-04 14:56:26 +0300

    Here we go!

---
+++ SolidTasks/LiskovSubstitution/LiskovSubstitution/Interfaces/IFlyable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution.Interfaces
+{
+    public interface IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("It's flying?");
+        }
+    }
+}
---
+++ SolidTasks/LiskovSubstitution/LiskovSubstitution/Vehicles/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution.Vehicles
+{
+    public class Vehicle
+    {
+        public string Car { get; }
+        public string Motorcycle { get; }
+        public string Airplane { get; }
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("The engine is starting");
+        }
+    }
+}
