dbee1fa9419d791cd12ba403fc54a2b9a9d8c414
VHarbar <vharbar64@gmail.com>
2023-04-12 12:43:28 +0300

    A lecture about delegates events and lamba has been aded

---
+++ Delegats-events-lambda/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegats_events_lambda
+{
+    public class Bank
+    {
+        public delegate void BankHandler(string message);
+        public event BankHandler? Notify;
+        public Bank(int sum) => Sum = sum;
+        public int Sum { get; private set; }
+        public void Put(int sum)
+        {
+            Sum += sum;
+            Notify?.Invoke($"Money was add: {sum}");
+        }
+        public void Take(int sum)
+        {
+            if(Sum >= sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke($"Money take: {sum}");
+            }
+            else
+            {
+                Notify?.Invoke($"Not enough money");
+            }
+        }
+    }
+}
---
+++ Delegats-events-lambda/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Delegats_events_lambda
+{
+    internal class Program
+    {
+        delegate void Message(string name);
+        delegate int Operation(int x, int y);
+        delegate T Operation<T, K>(K val);
+        static void Main(string[] args)
+        {
+            Message mes;
+            mes = Hello;
+            mes += HowAreYou;
+            mes("Vadim");
+
+            Operation oper = Add;
+            oper += Multiply;
+            int result = oper.Invoke(5, 10);
+            Console.WriteLine(result);
+            DoSomeThings(5, 10, Add);
+            /////////
+            void Hello(string name) => Console.WriteLine($"Hello {name} ");
+            void HowAreYou(string name) => Console.WriteLine($"How are you {name}");
+            int Add(int a, int y) => a + y;
+            int Multiply(int a, int b) => a * b;
+            ///////
+            void DoSomeThings(int a, int b, Operation op)
+            {
+                Console.WriteLine(op(a, b));
+            }
+            ////////
+            Bank bank = new Bank(300);
+            bank.Notify += displayMessage;
+            bank.Notify += displayRedMessage;
+
+            bank.Put(100);
+            bank.Take(1000);
+
+
+            void displayMessage(string message) => Console.WriteLine(message);
+            void displayRedMessage(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+            }
+
+        }
+    }
+}
